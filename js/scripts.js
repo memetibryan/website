5911c5d736407b5a29f10eb6a4f27f86b5328fef
memetibryan <memetibryan@gmail.com>
2017-04-26 17:54:30 +0300

    initial image toggle using jQuery

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,7 @@
-$(document).ready(function() {
-      $("p").click(function() {
-        alert("This is an paragraph.");
-      });
+jQuery("img").click(function() {
+  alert("This is an image.");
+});
+
+jQuery("here").click(function() {
+  jQuery(".img").toggle();
+});
